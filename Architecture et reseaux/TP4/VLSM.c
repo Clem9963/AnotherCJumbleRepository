67b6ea79f5e0ee6f0349514c17f23bb0f2153ba2
Clement_Dif <clementdt99@gmail.com>
2017-12-24 22:55:06 +0100

    Fin de l'écriture du code pour le TP 4 en Architecture & Réseaux.

--- Architecture et reseaux/TP4/VLSM.c
+++ Architecture et reseaux/TP4/VLSM.c
@@ -18,6 +18,9 @@ void afficheTab(int *tab, int taille);
 void extraire_adresse(char *adresse_cidr, char *adresse_dest);
 void dec_en_bin(char *adresse_dec, char *adresse_bin);
 void bin_en_dec(char *adresse_bin, char *adresse_dec);
+void init(char *adresse_bin);
+void addition(char *adresse_bin_src, char *adresse_bin_dest);
+void copie(char *adresse_bin_src, char *adresse_bin_dest);
 
 /* Calculs annexes */
 int calcule_nombre_bits(int nb_hotes);
@@ -27,11 +30,18 @@ int main(int argc, char *argv[])
 	/* Le programme prend un réseau en notation CIDR ainsi que la taille des sous-réseaux demandés. */
 	char adr_r_principal[TAILLE_ADR_CIDR];
 	char adr_r_principal_sans_cidr[TAILLE_ADR];
+	char adr_r_principal_bin_backup[TAILLE_ADR_BIN];
 	char adr_r_principal_bin[TAILLE_ADR_BIN];
+	char adr_hotes[TAILLE_ADR_BIN];
 	char temp[TAILLE_ADR_BIN];
-	int nb_sous_reseaux;
-	int *tailles_reseaux;
+
+	int nb_sous_reseaux = 0;
+	int nb_bits = 0;
+	int success = TRUE;
+	int masque = 0;
+	int *tailles_reseaux = NULL;
 	int i = 0;
+	int j = 0;
 
 	/* Test des inputs */
 	if (argc < 3)
@@ -42,6 +52,11 @@ int main(int argc, char *argv[])
 	if (strlen(argv[1]) > 18)
 	{
 		fprintf(stderr, "Adresse fournie incorrecte\n");
+		exit(EXIT_FAILURE);
+	}
+	if (strchr(adr_r_principal, '/') == NULL)
+	{
+		fprintf(stderr, "Le masque n'est pas renseigné\n");
 		exit(EXIT_FAILURE);
 	}
 
@@ -57,13 +72,69 @@ int main(int argc, char *argv[])
 	strcpy(adr_r_principal, argv[1]);
 
 	trierTableau(tailles_reseaux, nb_sous_reseaux);
-	afficheTab(tailles_reseaux, nb_sous_reseaux);
 	extraire_adresse(adr_r_principal, adr_r_principal_sans_cidr);
 	dec_en_bin(adr_r_principal_sans_cidr, adr_r_principal_bin);
-	printf("%s\n", adr_r_principal_sans_cidr);
-	printf("%s\n", adr_r_principal_bin);
+	dec_en_bin(adr_r_principal_sans_cidr, adr_r_principal_bin_backup);
 
-	printf("%d\n", calcule_nombre_bits(63));
+	masque = atoi(strchr(adr_r_principal, '/') + 1);
+	printf("%d\n", masque);
+
+	printf("N° sous-réseau\t");
+	printf("Masque du sous-réseau\t");
+	printf("Adr réseau\t");
+	printf("Adr 1er hôte\t");
+	printf("Adr dernier hôte\t");
+	printf("Adr broadcast\n");
+
+	for (i = 0; i < nb_sous_reseaux; i++)
+	{
+		printf("\t%d\t", i + 1);
+		
+		nb_bits = calcule_nombre_bits(tailles_reseaux[i]) - 1;
+		printf("\t/%d\t\t", 32 - (nb_bits+1));
+
+		printf("%s\t", adr_r_principal_sans_cidr);
+		
+		init(temp);
+		copie(adr_r_principal_bin, adr_hotes);
+		temp[TAILLE_ADR_BIN - 2] = '1';
+		addition(temp, adr_hotes);
+		bin_en_dec(adr_hotes, adr_r_principal_sans_cidr);
+		printf("%s\t", adr_r_principal_sans_cidr);
+
+		init(temp);
+		copie(adr_r_principal_bin, adr_hotes);
+		for (j = 0; j < nb_bits; j++)
+		{
+			temp[TAILLE_ADR_BIN - (3 + j)] = '1';
+		}
+		addition(temp, adr_hotes);
+		bin_en_dec(adr_hotes, adr_r_principal_sans_cidr);
+		printf("%s\t\t", adr_r_principal_sans_cidr);
+
+		adr_hotes[TAILLE_ADR_BIN - 2] = '1';
+		bin_en_dec(adr_hotes, adr_r_principal_sans_cidr);
+		printf("%s\n", adr_r_principal_sans_cidr);
+
+		for (j = 0; j < masque; j++)
+		{
+			success = success && (adr_r_principal_bin_backup[j] == adr_hotes[j]); 
+		}
+
+		init(temp);
+		temp[TAILLE_ADR_BIN - (3 + nb_bits)] = '1';
+		addition(temp, adr_r_principal_bin);
+		bin_en_dec(adr_r_principal_bin, adr_r_principal_sans_cidr);
+	}
+
+	if (success)
+	{
+		printf("\n\nDécoupage réussi\n");
+	}
+	else
+	{
+		printf("\n\nDécoupage échoué\n");
+	}
 
 	return EXIT_SUCCESS;
 }
@@ -180,4 +251,39 @@ int calcule_nombre_bits(int nb_hotes)
 	}
 
 	return i;
+}
+
+void init(char *adresse_bin)
+{
+	int i = 0;
+
+	for (i = 0; i < TAILLE_ADR_BIN - 1; i++)
+	{
+		adresse_bin[i] = '0';
+	}
+}
+
+void addition(char *adresse_bin_src, char *adresse_bin_dest)
+{
+	int i = 0;
+	int nb = 0;
+	int retenue = 0;
+
+	for (i = TAILLE_ADR_BIN - 2; i >= 0; i--)
+	{
+		nb = (adresse_bin_src[i] - '0') + (adresse_bin_dest[i] - '0') + retenue;
+		retenue = nb / 2;
+		nb %= 2;
+		adresse_bin_dest[i] = nb + '0';
+	}
+}
+
+void copie(char *adresse_bin_src, char *adresse_bin_dest)
+{
+	int i = 0;
+
+	for (i = 0; i < TAILLE_ADR_BIN; i++)
+	{
+		adresse_bin_dest[i] = adresse_bin_src[i];
+	}
 }
--- Architecture et reseaux/TP4/divisionSousReseau.c
+++ Architecture et reseaux/TP4/divisionSousReseau.c
@@ -11,24 +11,24 @@ int main(int argc, char *argv[]){
      
      if(argc != 3)
      {
-          fprintf(stderr, "Veuillez rentrer une IP/cidr et un entier k\n");
+          printf("Veuillez rentrer une IP/cidr et un entier k\n");
           exit(-1);
      }
      else if(strlen(argv[1]) > 18)
      {
-          fprintf(stderr, "adresse ip/cidr trop long ! \n");
+          printf("adresse ip/cidr trop long ! \n");
           exit(-1);
      }
      
 
      char adresseIPDec[15];
      int nbCidr = 0;
+     int nbBits;
      int k = atoi(argv[2]);
-     int nbBits = 0;
           
      //recuperation de l'adresse ip et du nombre cidr
      recupEntree(adresseIPDec, &nbCidr, argv[1]);
-     
+
      nbBits = verification(nbCidr, k);
 
      //affiche les sous reseaux
@@ -50,67 +50,74 @@ void recupEntree(char *adresseIPDec, int *nbCidr, char *argv){
      }
 
      *nbCidr = (argv[i + 1] - '0') * 10 + (argv[i + 2] - '0');
-     
-     if(*nbCidr > 32)
-     {
-   	 	fprintf(stderr, "cidr trop grand !\n");
-   	 	exit(-1);
-   	 }
-}
-
-int verification(int nbCidr, int k){
-	int i;
-	int nb = 0;
-	
-	nb = 32 - nbCidr;
-	
-	for (i = 1; i < nb; i++)
-	{	
-		if(pow(2, i) >= k)
-			return i;
-	}
-	
-	fprintf(stderr, "Impossible de diviser en autant de sous reseau\n");
-	exit(-1);
 }
 
 void divisionReseau(char *adresse, int k, int nbCidr, int nbBits){
-     int i, j, indice = 0, compteur = 0;
+     int i = 0, j = 0, indice = 0, compteur = 0;
+     int part = (int) 255/k;
      int nbBitsRestant = 32 - (nbCidr + nbBits);
      char masque[32];
-     char hoteDec[15];
-     
+
      if (nbBitsRestant < 0 || pow(2, nbBitsRestant) < k)
      {
-     	fprintf(stderr, "impossible de diviser !\n");
+          fprintf(stderr, "impossible de diviser !\n");
      }
      else
      {
-		 printf("bits restants : %d\n", nbBitsRestant);
-		 
-		 //creation du masque
-		 for (i = 0; i < 32; i++)
-		 {
-		 	if(i == 8 || i == 16 || i == 24)
-		 		masque[i] = '.';
-		 	else if(i < (32 - nbBitsRestant))
-		 		masque[i] = '1';
-		 	else
-		 		masque[i] = '0';
-		 
-		 }
+          printf("bits restants : %d\n", nbBitsRestant);
 
-		 //recupere l'indice du troisieme point dans l'adresse
+          //creation du masque
+          for (i = 0; i < 35; i++)
+          {
+               if(i == 8 || i == 17 || i == 26)
+               {
+                    compteur++;
+                    masque[i] = '.';
+               }
+               else if(i < (32 - nbBitsRestant) + compteur)
+                    masque[i] = '1';
+               else
+                    masque[i] = '0';
+          }
+          printf("masque : %s\n", masque);
 
+          //recupere l'indice du troisieme point dans l'adresse
+          compteur = 0;
+          while(compteur != 3)
+          {
+               if(adresse[indice] == '.')
+                    compteur++;
+               indice++;
+          }
 
-		 for (i = 0; i < k; i++)
-		 {
-		       printf("\n %i sous reseau : ", i + 1);
-		       for(j = 0; j < indice; j++)
-		            printf("%c", adresse[j]);
-		       printf("%d/%d", (int)pow(2, nbBitsRestant) * i, (int)nbCidr + nbBits);
-
-		 }
-		 printf("\n\n");
+          for (i = 0; i < k; i++)
+          {
+               //verifie si on depasse pas 255
+               if(i * part + i <= 255){
+                    printf("\n %i sous reseau : ", i + 1);
+                    for(j = 0; j < indice; j++)
+                         printf("%c", adresse[j]);
+                    printf("%d/%d", i * part + i, nbCidr + 2);
+               }
+               else
+                    break;
+          }
+          printf("\n\n");
      }
+}
+
+int verification(int nbCidr, int k){
+     int i;
+     int nb = 0;
+     
+     nb = 32 - nbCidr;
+     
+     for (i = 1; i < nb; i++)
+     {    
+          if(pow(2, i) >= k)
+               return i;
+     }
+     
+     fprintf(stderr, "Impossible de diviser en autant de sous reseau\n");
+     exit(-1);
 }
